9d7893212738aabef3f4ede67497e364b5b45a5b
mahmudexpert <71225015+mahmudexpert@users.noreply.github.com>
2023-06-01 15:02:08 +0600

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+// mysonry js //	
+var $grid = $('.grid').isotope({
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    // use outer width of grid-sizer for columnWidth
+    columnWidth: '.grid-sizer'
+  }
+})
